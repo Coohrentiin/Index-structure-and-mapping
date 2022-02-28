e75707e46e6404a3971c9bf8807048475e085882
Corentin SOUBEIRAN <corentin.soubeiran@ensta-paristech.fr>
2022-02-28 17:49:18 +0100

    class BWT

---
+++ BWT.py
@@ -0,0 +1,18 @@
+import numpy as np
+import time 
+import memory_profiler
+
+
+class BWT(object):
+    def __init__(self,text):
+        return(0)
+    
+    def compute_sufixe_array(self):
+        return(0)
+    
+    def construct_bwt(self):
+        return(0)
+    
+    def bwt_search(self):
+        return(0)
+    
